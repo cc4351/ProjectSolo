7d70ac15814b8b48598187a9d10204c142a431a5
Chen Chen <cc4351@columbia.edu>
2020-06-26 14:16:32 -0400

    update

--- graph3.js
+++ graph3.js
@@ -1,26 +1,28 @@
+//initialize svgs and bind with DOM elements
 var svg0 = d3.select("#growth").append("svg")
 				 .attr("width", $(growth).width())
-				 .attr("height", 800)
+				 .attr("height", $(growth).height())
 				 .attr("id", "svg0");
 
 
 var svg1 = d3.select("#graph1").append("svg")
 				 .attr("width", $(graph1).width())
-				 .attr("height", 800)
+				 .attr("height", $(graph1).height())
 				 .attr("id", "svg1");
 		
 
 var svg2 = d3.select("#graph2").append("svg")
 				 .attr("width", $(graph2).width())
-				 .attr("height", 800)
+				 .attr("height", $(graph2).height())
 				 .attr("id", "svg2");
 
 var svg3 = d3.select("#graph3").append("svg")
 				 .attr("width", $(graph3).width())
-				 .attr("height", 800)
+				 .attr("height", $(graph3).height())
 				 .attr("id", "svg3");
 
 
+//nodeCoordinates
 var nodeData = [{ x: 55, y: 59 },
 				{ x: 48, y: 81 },
 				{ x: 51, y: 34 },
@@ -39,9 +41,8 @@ var nodeData = [{ x: 55, y: 59 },
 			  ];
 
 
-
 //if necessary, change the identifier to id instead of index
-//centralized
+//centralized connections
 var c1 = [{from: 0, to: 1},
 			   {from: 0, to: 2}, 
 			   {from: 0, to: 3},
@@ -58,7 +59,7 @@ var c1 = [{from: 0, to: 1},
 			   {from: 0, to: 14}]
 
 
-//decentralized
+//decentralized connections
 var c2 = [{from: 0, to: 1},
 			{from: 0, to: 2},
 			{from: 0, to: 3},
@@ -78,7 +79,7 @@ var c2 = [{from: 0, to: 1},
 			];
 
 
-//distributed
+//distributed connections
 var c3 = [{from: 0, to: 2},
 			{from: 0, to: 10},
 			{from: 0, to: 5},
@@ -113,11 +114,24 @@ var c3 = [{from: 0, to: 2},
 			{from: 10, to: 5}];
 
 
-function cLine(connect, nData, idx){
+var mulFactor = 3;
+
+//multiply the xy coordinates by mulFactor
+function adjustCoord(nodeData, mulFactor){
+	for(let i =0; i<nodeData.length; i++){
+		nodeData[i].x *= mulFactor;
+		nodeData[i].y *= mulFactor;
+	}
+	return nodeData;
+}
+
+//create lines and adjust node locations
+function cLine(connect, nData, idx, mulFactor=1){
 	var lineData = [];
 	var nodeData = nData;
 	var sumLen = 0;
-	for(i = 0; i<connect.length; i++){
+	nodeData = adjustCoord(nodeData, mulFactor);
+	for(let i = 0; i<connect.length; i++){
 		var pt1, pt2, x1, x2, y1, y2, len, tmp;
 		pt1 = nodeData[connect[i].from];
 		pt2 = nodeData[connect[i].to];
@@ -125,11 +139,13 @@ function cLine(connect, nData, idx){
 		y1 = pt1.y;
 		x2 = pt2.x;
 		y2 = pt2.y;
+		//calculate full length of link
 		len = Math.sqrt((x1-x2)**2 + (y1-y2)**2);
-		sumLen += len;
+		sumLen += len;//for the cumulative length
+		//link-related information
 		tmp = {from: idx+"_"+"n"+connect[i].from, to: idx+"_"+"n"+connect[i].to, x1: x1, y1: y1, x2: x2, y2: y2, len: len, id: idx+"_"+"l"+i};
 		lineData.push(tmp);
-
+		//add the link information to nodes as well
 		if(pt1.hasOwnProperty("link")){
 			pt1.link.push(idx+"_"+"l"+i);
 		}else{
@@ -142,8 +158,6 @@ function cLine(connect, nData, idx){
 			pt2.link = [idx+"_"+"l"+i];
 			pt2.color = "red";
 		}
-		d3.select('#'+tmp.from).attr("fill", "red");
-		d3.select('#'+tmp.to).attr("fill", "red");
 	}
 	return [lineData, nodeData, sumLen];
 }
@@ -153,23 +167,19 @@ var lineData1, nodeData1, lineData2, nodeData2, lineData3, nodeData3, sumLen1, s
 nodeData1 = JSON.parse(JSON.stringify(nodeData));
 nodeData2 = JSON.parse(JSON.stringify(nodeData));
 nodeData3 = JSON.parse(JSON.stringify(nodeData));
-[lineData1, nodeData1, sumLen1] = cLine(c1, nodeData1, "svg1");
-[lineData2, nodeData2, sumLen2] = cLine(c2, nodeData2, "svg2");
-[lineData3, nodeData3, sumLen3] = cLine(c3, nodeData3, "svg3");
-
-sumLen1 *= 3;
-sumLen2 *= 3;
-sumLen3 *= 3; 
+[lineData1, nodeData1, sumLen1] = cLine(c1, nodeData1, "svg1", mulFactor);
+[lineData2, nodeData2, sumLen2] = cLine(c2, nodeData2, "svg2", mulFactor);
+[lineData3, nodeData3, sumLen3] = cLine(c3, nodeData3, "svg3", mulFactor);
 
 
-//for svg0
-var centroid = 0;
+//for svg0 --> spiral appearance of nodes 
+var centroid = 0;//center of the spiral
 function cDist(centroid, nData){
 	var len;
 	var max = 0;
 	var cX = nData[centroid].x;
 	var cY = nData[centroid].y;
-	for(i = 0; i<nData.length; i++){
+	for(let i = 0; i<nData.length; i++){
 		len = Math.sqrt((nData[i].x-cX)**2 + (nData[i].y-cY)**2);
 		max = len>max?len:max;
 		nData[i].cDist = len;
@@ -179,6 +189,7 @@ function cDist(centroid, nData){
 }
 
 var nodeData0 = JSON.parse(JSON.stringify(nodeData));
+nodeData0 = adjustCoord(nodeData0, mulFactor);
 var maxDist;
 [nodeData0, maxDist] = cDist(centroid, nodeData0);
 //sort nodeData0 by distance from center
@@ -186,19 +197,19 @@ nodeData0.sort(function (a, b){
     return a.cDist-b.cDist;
 });
 
-//solve this *3 step
+
 var lineAttrs = {
-	x1: function(d){ return d.x1*3;},
-	x2: function(d){ return d.x2*3;},
-	y1: function(d){ return d.y1*3;},
-	y2: function(d){ return d.y2*3;},
+	x1: function(d){ return d.x1;},
+	x2: function(d){ return d.x2;},
+	y1: function(d){ return d.y1;},
+	y2: function(d){ return d.y2;},
 	from: function(d){ return d.from;},
 	to: function(d){ return d.to;},
 	stroke: "black",
 	"stroke-width": 2,
 	opacity: 0.5,
 	id: function(d){return d.id;},
-	standardLen: function(d){return d.len*3;}
+	standardLen: function(d){return d.len;}
 };
 
 function drawLine(svgC, lData, lAttrs){
@@ -215,8 +226,8 @@ var lines2 = drawLine(svg2, lineData2, lineAttrs);
 var lines3 = drawLine(svg3, lineData3, lineAttrs);
 
 var circleAttrs = {
-	  cx: function(d) { return d.x*3; },
-	  cy: function(d) { return d.y*3; },
+	  cx: function(d) { return d.x; },
+	  cy: function(d) { return d.y; },
 	  id: function(d, i){return "n"+i;},
 	  fill: function(d){
 		  if (d.color){
@@ -253,11 +264,12 @@ var nodes3 = drawCircle(svg3, nodeData3, circleAttrs, "svg3");
 
 nodes0.attr("opacity", 0);
 
+//-----------------END OF NODE/LINK DRAWING-----------------------------
+
 //requires some fixing later
 var base0 = $("#intro").show().length==0?0:$(intro).height();
 var base1 = $("#growth").show().length==0?base0:base0+$(growth).height();
-//console.log(base0);
-var frameHeight = d3.select("#graph1").node().scrollHeight;
+var frameHeight = $("#graph1").height();
 var scroll_length = frameHeight/4;
 var lineScale = d3.scale.linear()//ref: https://www.dashingd3js.com/d3js-scales
 				  .domain([0, scroll_length])
@@ -267,7 +279,6 @@ var scrollTop = 0;
 var newScrollTop = 0;
 var currentScrollTop = d3.select('#currentScrollTop');
 var nodeGray = "rgb(100, 100, 100)";
-// var nodeGreen = "#718477";
 var nodeGreen = "green";
 
 window.addEventListener('scroll', function(e){
@@ -276,13 +287,14 @@ window.addEventListener('scroll', function(e){
 
 //resetting all params - for resizing
 var setDimensions = function() {
-  frameHeight = d3.select("#graph1").node().scrollHeight;
+  frameHeight = $("#graph1").height();
   scroll_length =frameHeight/4;
   lineScale.domain([0, scroll_length]);		  
 }
 
 var segment = $(growth).height()/(nodes0[0].length+7);
 var idx, tol;
+//set the threshold for appearance for each node in svg0
 nodes0.each(function(){
 	idx = parseInt(this.id.split('_')[1].replace('n', ''));
 	tol = idx*segment;
@@ -292,10 +304,8 @@ nodes0.each(function(){
 var bluePts = [6, 10];
 var numV = nodeData.length;
 //implement a DFS path search here
-//ref: https://www.geeksforgeeks.org/count-possible-paths-two-vertices/
 //ref: https://www.geeksforgeeks.org/count-total-ways-to-reach-destination-from-source-in-an-undirected-graph/?ref=leftbar-rightbar
 function countPaths(conn, numV, i, sink, visited){
-	// countPaths(conn, numV, src, sink, visited); 
 	if (i == sink) { 
         return 1; 
     }
@@ -317,7 +327,7 @@ function countPaths(conn, numV, i, sink, visited){
 //conn -> connection array
 //numV -> int, num of nodes in the graph
 //twoEnds -> starting and ending point of the path search
-function pathSearch(connData, numV, twoEnds, blockList=[]){
+function pathSearch(connData, numV, twoEnds, blockList=new Set()){
 	//initialized the connectivity matrix
 	var conn = [];
 	for(let i=0; i<numV; i++) {
@@ -326,7 +336,7 @@ function pathSearch(connData, numV, twoEnds, blockList=[]){
 	}
 	for(let i=0; i<connData.length;i++){
 		var link = connData[i];
-		if(blockList.includes(link.to) || blockList.includes(link.from))
+		if(blockList.has(link.to) || blockList.has(link.from))
 			continue;
 		conn[link.to][link.from] = 1;
 		conn[link.from][link.to] = 1;
@@ -340,47 +350,64 @@ function pathSearch(connData, numV, twoEnds, blockList=[]){
     return countPaths(conn, numV, src, sink, visited); 
 }
 
-document.getElementById("path1").innerHTML = pathSearch(c1, numV, bluePts, []);
-document.getElementById("path2").innerHTML = pathSearch(c2, numV, bluePts, []);
-document.getElementById("path3").innerHTML = pathSearch(c3, numV, bluePts, []);
+document.getElementById("path1").innerHTML = pathSearch(c1, numV, bluePts);
+document.getElementById("path2").innerHTML = pathSearch(c2, numV, bluePts);
+document.getElementById("path3").innerHTML = pathSearch(c3, numV, bluePts);
 
 
-//refactor this code
 var render = function() {
   if (scrollTop != newScrollTop) {
 	scrollTop = newScrollTop//update scrollTop, needs to be done after container reacts to scroller.scroll
-	function rescale(lines=[], connect=[], svgC, sumLen = 0, base, bottom = base+800, max=0){
-		var dif = scrollTop - base;
-		if(svgC == svg0){
-			dif +=40;
-		}
-		svgC.attr('transform','translate(' + svgC.attr("width")/8 + ',' + dif+ ')');
-		if(svgC == svg0){
-			nodes0.each(function(){
-				if(scrollTop >= d3.select(this).attr("tol")){
-					d3.select(this).attr("opacity", 0.8).attr("fill", "red");
-				}
-				if(scrollTop < d3.select(this).attr("tol")){
-					d3.select(this).attr("opacity", 0.1).attr("fill", "black");
-				}
-				
-			});
+
+	function introScale(svgC, top, max=0){
+		svgC = "#" + svgC;
+		var bottom = $(svgC).height() + top;
+		var dif = scrollTop - top + 40;
+		d3.select(svgC).attr('transform','translate(' + d3.select(svgC).attr("width")/8 + ',' + dif+ ')');
+
+		nodes0.each(function(){
+			if(scrollTop >= d3.select(this).attr("tol")){
+				d3.select(this).attr("opacity", 0.8).attr("fill", "red");
+			}
+			if(scrollTop < d3.select(this).attr("tol")){
+				d3.select(this).attr("opacity", 0.1).attr("fill", "black");
+			}
 			
-			return;
-		}
-		var scales = lineScale(scrollTop-base);
-		if(scrollTop>= base && scrollTop < bottom){
+		});
+		
+	}
+
+	function rescale(lines=[], connect=[], svgC, sumLen = 0, top){
+		svgC = "#" + svgC;
+		var bottom = $(svgC).height() + top;
+		var dif = scrollTop - top;
+		//translate the graph down as the user scroll
+		d3.select(svgC).attr('transform','translate(' + d3.select(svgC).attr("width")/8 + ',' + dif+ ')');
+		var scales = lineScale(scrollTop-top);//scaling factor
+		//scale the sum of length by the scaling factor
+		if(scrollTop>= top && scrollTop < bottom){
 			var validSum = scales*sumLen;
 		}
-		var header = svgC.attr("id")+"_n";
-		if(scrollTop - base>scroll_length && scrollTop <bottom){
-			bluePts.forEach(ele => d3.select("#"+header+ele).attr("fill", nodeGreen).attr("bFlag", 1));
+		var header = d3.select(svgC).attr("id")+"_n";
+
+		//color the two designated dots green if the graph is currently in range
+		//otherwise color them red
+		if(scrollTop - top>scroll_length && scrollTop <bottom){
+			bluePts.forEach(ele => {
+				var oriColor = d3.select("#"+header+ele).attr("fill");
+				if(oriColor != nodeGray){d3.select("#"+header+ele).attr("fill", nodeGreen);}
+				d3.select("#"+header+ele).attr("bFlag", 1);
+			});
 			
 		}else{
-			bluePts.forEach(ele => d3.select("#"+header+ele).attr("fill", "red").attr("bFlag", 0));
+			bluePts.forEach(ele => {
+				var oriColor = d3.select("#"+header+ele).attr("fill");
+				if(oriColor != nodeGray){d3.select("#"+header+ele).attr("fill", "red");}
+				d3.select("#"+header+ele).attr("bFlag", 0);
+			});
 		}
 
-		//check if this part could be optimized by translate-transform
+		//scale each line by the scaling factor
 		lines.each(function(){
 			var idx = parseInt(this.id.split('_')[1].replace('l', ''));
 			var graph = this.id.split('_')[0];
@@ -402,7 +429,7 @@ var render = function() {
 							.attr('stroke-width', 2)
 							.attr("opacity", 0.5)
 							.attr("pathlength", newLen);
-			
+			//if any of the two nodes is gray, set opacity to 0
 			if(data1.attr("fill") == nodeGray || data2.attr("fill") == nodeGray ){
 				d3.select(this).attr('opacity', 0);
 				if(typeof validSum != "undefined"){
@@ -411,7 +438,8 @@ var render = function() {
 			}
 
 		});
-
+        
+        //update the total length of wire for the graph in scope
 		if(typeof validSum != "undefined"){
 			var elements = document.getElementsByClassName("scrollTop");
 			for(i = 0;i<elements.length; i++){
@@ -421,11 +449,10 @@ var render = function() {
 		
 	}
 
-    //needs some restructuring here
-	rescale([], [], svg0, 0, base0, base1, maxDist);
-	rescale(lines1, c1, svg1, sumLen1, base1, frameHeight+base1);
-	rescale(lines2, c2, svg2, sumLen2, frameHeight+base1, frameHeight*2+base1);
-	rescale(lines3, c3, svg3, sumLen3, frameHeight*2+base1);
+	introScale("svg0", base0, maxDist);
+	rescale(lines1, c1, "svg1", sumLen1, base1);
+	rescale(lines2, c2, "svg2", sumLen2, frameHeight+base1);
+	rescale(lines3, c3, "svg3", sumLen3, frameHeight*2+base1);
       
 	currentScrollTop.text(scrollTop)//update currentScrollTop?
   }
@@ -439,31 +466,34 @@ window.onresize = setDimensions
 
 
 //click event
+//requires refactoring
+//problem1: click() function is too long => possible to break into multiple reusable parts?
+//minor improvement: add some comments for readability
+//question(?): possible to make links clickable as well? or is it a bad idea? 
 function click(nodes, nData){
 	var nd, node, ind, data;
 	var seq;
-	var prevGray = []
+	var prevGray = new Set();
 	nodes.each(function(){
 		d3.select(this).on("click", function(){
 			node = this.id.split('_')[1];
+			//get the corresponding svg name
 			if(seq == null){ 
 				seq = this.id.split('_')[0].replace('svg', ''); 
 			}
 			ind = parseInt(String(node).replace('n', ''));
 			data = nData[ind];
+			//color alternating between green/red and gray
 			if(bluePts.includes(ind) && d3.select(this).attr("bFlag") == 1){
 				var newColor = d3.select(this).attr("fill") == nodeGreen? nodeGray:nodeGreen;
 			}else{
 				var newColor = d3.select(this).attr("fill") == "red"? nodeGray:"red";
 			}
+			//update the prevGray set according to the new color
 			if(newColor == nodeGray){
-				prevGray.push(ind);
+				prevGray.add(ind);
 			}else{
-				//ref: https://stackoverflow.com/a/5767357/12144813
-				const index = prevGray.indexOf(ind);
-				if (index > -1) {
-				  prevGray.splice(index, 1);
-				}
+				prevGray.delete(ind);
 			}
 			d3.select(this).attr("fill", newColor);
 			var pt1, pt2, newOpacity;
@@ -471,22 +501,22 @@ function click(nodes, nData){
 				var links = data["link"];
 			
 				for(i=0; i<links.length; i++){
+					//new opacity for links associated with the clicked node
 					var lk = d3.select("#" + links[i]);
                     pt1 = d3.select("#" + lk.attr("from"));
 					pt2 = d3.select("#"+ lk.attr("to"));
-					if(pt1.attr("fill") == nodeGray || pt2.attr("fill") == nodeGray){
-						newOpacity = 0;
-					}else{
-						newOpacity =lk.attr("opacity")==0.5?0:0.5;
-					}
+					var oldOpacity = lk.attr("opacity");
+					var decision = (pt1.attr("fill") == nodeGray || pt2.attr("fill") == nodeGray);
+					newOpacity = decision?0:0.5;
 					lk.attr("opacity", newOpacity);
+					//update the sum of total length
 					var tmpLen = parseFloat(lk.attr("pathlength"));
 					var scrollOne = document.getElementsByClassName("scrollTop");
 					var numericalSum = parseFloat(scrollOne[0].innerHTML);
-					// console.log(numericalSum);
-					if(newOpacity == 0.5){
+
+					if(newOpacity>oldOpacity){
 						numericalSum += tmpLen;
-					}else{
+					}else if(newOpacity<oldOpacity){
 						numericalSum -= tmpLen;
 					}
 					for(let i=0; i<scrollOne.length; i++){
@@ -498,7 +528,6 @@ function click(nodes, nData){
 			}
 			var id = "path" + seq;
 			var tmp = [c1, c2, c3];
-			console.log(prevGray);
 			document.getElementById(id).innerHTML = pathSearch(tmp[seq-1], numV, bluePts, prevGray);
 
 		});
